418538e0c1e45175df68560cb044ec84013f7fcb
swampmonster <>
2020-10-26 21:46:15 +0100

    adding support for layout 2a

--- keyboards/rominronin/katana60/rev1/rev1.h
+++ keyboards/rominronin/katana60/rev1/rev1.h
@@ -17,21 +17,45 @@
 
 #include "quantum.h"
 
-// This a shortcut to help you visually see your layout.
-// The following is an example using the Planck MIT layout
-// The first section contains all of the arguments
-// The second converts the arguments into a two-dimensional array
-#define LAYOUT( \
-      k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b, k4c, k4d, k4e, \
-      k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, \
-      k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, \
-      k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e, \
-      k00, k01, k02, k03, k04, k05, k06,           k09, k0a, k0b, k0c, k0d \
-) \
-{ \
-    { k00, k01, k02, k03, k04, k05, k06, KC_NO, KC_NO, k09, k0a, k0b, k0c, k0d, KC_NO  }, \
-    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a, k1b, k1c, k1d, k1e  }, \
-    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a, k2b, k2c, k2d, KC_NO  }, \
-    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a, k3b, k3c, k3d, KC_NO  }, \
-    { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a, k4b, k4c, k4d, k4e  }, \
+#define XXX KC_NO
+
+/* LAYOUT_2_a
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+ * │00 │01 │02 │03 │04 │05 │06 │07 │08 │09 │0A │0B │0C │0D │0E │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
+ * │10   │11 │12 │13 │14 │15 │16 │18 │19 │1A │1B │1C │1D │1E   │
+ * ├────┬┴──┬┴──┬┴──┬┴──┬┴──┬┴──┬┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬────┤
+ * │20  │21 │22 │23 │24 │25 │26 │ │28 │29 │2A │2B │2C │2D │2E  │
+ * ├───┬┴──┬┴──┬┴──┬┴──┬┴──┬┴──┬┴─┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬───┤
+ * │30 │31 │32 │33 │34 │35 │36 │37 │38 │39 │3A │3B │3C │3D │3E │
+ * ├───┴─┬─┴──┬┴───┴┬──┴───┴───┼───┼───┴───┼───┼───┼───┼───┼───┤
+ * │41   │42  │43   │    44    │47 │  49   │4A │4B │4C │4D │4E │
+ * └─────┴────┴─────┴──────────┴───┴───────┴───┴───┴───┴───┴───┘
+ */
+/*#define LAYOUT_2_a( \
+    Q41,   Q42,  Q43,         Q44,     Q47,   Q49,    Q4A, Q4B, Q4C, Q4D, Q4E, \
+    Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37, Q38, Q39, Q3A, Q3B, Q3C, Q3D, Q3E, \
+    Q20,  Q21, Q22, Q23, Q24, Q25, Q26,    Q28, Q29, Q2A, Q2B, Q2C, Q2D,  Q2E, \
+    Q10,   Q11, Q12, Q13, Q14, Q15, Q16,  Q18, Q19, Q1A, Q1B, Q1C, Q1D,   Q1E, \
+    Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07, Q08, Q09, Q0A, Q0B, Q0C, Q0D, Q0E  \
+) { \
+    { Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07, Q08, Q09, Q0A, Q0B, Q0C, Q0D, Q0E }, \
+    { Q10, Q11, Q12, Q13, Q14, Q15, Q16, XXX, Q18, Q19, Q1A, Q1B, Q1C, Q1D, Q1E }, \
+    { Q20, Q21, Q22, Q23, Q24, Q25, Q26, XXX, Q28, Q29, Q2A, Q2B, Q2C, Q2D, Q2E }, \
+    { Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37, Q38, Q39, Q3A, Q3B, Q3C, Q3D, Q3E }, \
+    { XXX, Q41, Q42, Q43, Q44, XXX, XXX, Q47, XXX, Q49, Q4A, Q4B, Q4C, Q4D, Q4E }  \
+}*/
+#define LAYOUT_2_a( \
+    Q40,   Q41,  Q43,         Q44,     Q45,   Q46,    Q49, Q4A, Q4B, Q4C, Q4D, \
+    Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37, Q38, Q39, Q3A, Q3B, Q3C, Q3D, Q3E, \
+    Q20,  Q21, Q22, Q23, Q24, Q25, Q26,    Q27, Q28, Q29, Q2A, Q2B, Q2C,  Q2D, \
+    Q10,   Q11, Q12, Q13, Q14, Q15, Q16,  Q17, Q18, Q19, Q1A, Q1B, Q1C,   Q1D, \
+    Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07, Q08, Q09, Q0A, Q0B, Q0C, Q0D, Q0E  \
+) { \
+    { Q00, Q01, Q02, Q03, Q04, Q05, Q06, Q07, Q08, Q09, Q0A, Q0B, Q0C, Q0D, Q0E }, \
+    { Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18, Q19, Q1A, Q1B, Q1C, Q1D, XXX }, \
+    { Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, Q28, Q29, Q2A, Q2B, Q2C, Q2D, XXX }, \
+    { Q30, Q31, Q32, Q33, Q34, Q35, Q36, Q37, Q38, Q39, Q3A, Q3B, Q3C, Q3D, Q3E }, \
+    { Q40, Q41, XXX, Q43, Q44, Q45, Q46, XXX, XXX, Q49, Q4A, Q4B, Q4C, Q4D, XXX }  \
 }
+
